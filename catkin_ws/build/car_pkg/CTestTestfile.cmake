0e48a0ec9f32f3eaf9d07131dd377e5b24936f0f
rezk ahmed <rezksh55112@gmail.com>
2023-09-05 18:57:22 +0300

    create a map

---
+++ catkin_ws/build/car_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rezk/ROS_workspacws/robot_car/catkin_ws/src/car_pkg
+# Build directory: /home/rezk/ROS_workspacws/robot_car/catkin_ws/build/car_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
